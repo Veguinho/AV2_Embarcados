92da5ffb2c43fd08116f064aff68eb7ad066ac6b
veguinho <rafaelvr@al.insper.edu.br>
2019-05-13 19:02:46 -0300

    adc funcionando e printando

--- RTOS-LCD-maxTouch/MXT_EXAMPLE_USART1/src/main.c
+++ RTOS-LCD-maxTouch/MXT_EXAMPLE_USART1/src/main.c
@@ -84,13 +84,29 @@
  * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
  */
 
+//  typedef struct {
+// 	 const uint8_t *data;
+// 	 uint16_t width;
+// 	 uint16_t height;
+// 	 uint8_t dataSize;
+//  } tImage
+
 #include <asf.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "tfont.h"
 #include "conf_board.h"
 #include "conf_example.h"
 #include "conf_uart_serial.h"
+#include "ar.h"
+#include "soneca.h"
+#include "termometro.h"
+#include "digital521.h"
+
+
+
+
 
 /************************************************************************/
 /* LCD + TOUCH                                                          */
@@ -112,6 +128,25 @@ const uint32_t BUTTON_Y = ILI9488_LCD_HEIGHT/2;
 
 #define TASK_LCD_STACK_SIZE            (2*1024/sizeof(portSTACK_TYPE))
 #define TASK_LCD_STACK_PRIORITY        (tskIDLE_PRIORITY)
+
+#define TASK_POTENCIOMETRO_STACK_SIZE            (2*1024/sizeof(portSTACK_TYPE))
+#define TASK_POTENCIOMETRO_STACK_PRIORITY        (tskIDLE_PRIORITY)
+
+#define AFEC_CHANNEL_POT_SENSOR 0 //PD30
+
+/** Reference voltage for AFEC,in mv. */
+#define VOLT_REF        (3300)
+
+/** The maximal digital value */
+/** 2^12 - 1                  */
+#define MAX_DIGITAL     (4095)
+
+//Pot
+volatile uint32_t pot_ul_value;
+
+volatile uint32_t valor_potencia_convertida = 0;
+
+char bufferPotencia[32];
 
 typedef struct {
   uint x;
@@ -164,6 +199,8 @@ extern void vApplicationMallocFailedHook(void)
 	/* Force an assert. */
 	configASSERT( ( volatile void * ) NULL );
 }
+
+
 
 /************************************************************************/
 /* init                                                                 */
@@ -277,10 +314,97 @@ static void mxt_init(struct mxt_device *device)
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+static void AFEC_pot_callback(void)
+{
+	afec_channel_enable(AFEC0, AFEC_CHANNEL_POT_SENSOR);
+	afec_start_software_conversion(AFEC0);
+	pot_ul_value = afec_channel_get_value(AFEC0, AFEC_CHANNEL_POT_SENSOR);
+}
+
+static void config_POT(void){
+/*************************************
+   * Ativa e configura AFEC
+   *************************************/
+  /* Ativa AFEC - 0 */
+	afec_enable(AFEC0);
+
+	/* struct de configuracao do AFEC */
+	struct afec_config afec_cfg;
+
+	/* Carrega parametros padrao */
+	afec_get_config_defaults(&afec_cfg);
+
+	/* Configura AFEC */
+	afec_init(AFEC0, &afec_cfg);
+
+	/* Configura trigger por software */
+	afec_set_trigger(AFEC0, AFEC_TRIG_SW);
+
+	/* configura call back */
+	afec_set_callback(AFEC0, AFEC_INTERRUPT_EOC_0,	AFEC_pot_callback, 1);
+
+	/*** Configuracao específica do canal AFEC ***/
+	struct afec_ch_config afec_ch_cfg;
+	afec_ch_get_config_defaults(&afec_ch_cfg);
+	afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+	afec_ch_set_config(AFEC0, AFEC_CHANNEL_POT_SENSOR, &afec_ch_cfg);
+
+	/*
+	* Calibracao:
+	* Because the internal ADC offset is 0x200, it should cancel it and shift
+	 down to 0.
+	 */
+	afec_channel_set_analog_offset(AFEC0, AFEC_CHANNEL_POT_SENSOR, 0x200);
+
+	/***  Configura sensor de temperatura ***/
+	//struct afec_temp_sensor_config afec_pot_sensor_cfg;
+
+	//afec_temp_sensor_get_config_defaults(&afec_pot_sensor_cfg);
+	//afec_temp_sensor_set_config(AFEC0, &afec_pot_sensor_cfg);
+
+	/* Selecina canal e inicializa conversão */
+	afec_channel_enable(AFEC0, AFEC_CHANNEL_POT_SENSOR);
+}
+
+static int32_t convert_adc_to_pot(int32_t ADC_value){
+
+  int32_t ul_vol;
+  int32_t ul_temp;
+
+  /*
+   * converte bits -> tensão (Volts)
+   */
+	ul_vol = ADC_value * VOLT_REF / (float) MAX_DIGITAL;
+
+  /*
+   * According to datasheet, The output voltage VT = 0.72V at 27C
+   * and the temperature slope dVT/dT = 2.33 mV/C
+   */
+  
+  return(ul_vol);
+}
+
+void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
+	char *p = text;
+	while(*p != NULL) {
+		char letter = *p;
+		int letter_offset = letter - font->start_char;
+		if(letter <= font->end_char) {
+			tChar *current_char = font->chars + letter_offset;
+			ili9488_draw_pixmap(x, y, current_char->image->width, current_char->image->height, current_char->image->data);
+			x += current_char->image->width + spacing;
+		}
+		p++;
+	}
+}
 
 void draw_screen(void) {
 	ili9488_set_foreground_color(COLOR_CONVERT(COLOR_WHITE));
 	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+	ili9488_draw_pixmap(10,50,termometro.width,termometro.height,termometro.data);
+	ili9488_draw_pixmap(10,250,ar.width,ar.height,ar.data);
+	ili9488_draw_pixmap(ILI9488_LCD_WIDTH-soneca.width-5, ILI9488_LCD_HEIGHT-soneca.height-5,soneca.width,soneca.height,soneca.data);
+
 }
 
 void draw_button(uint32_t clicked) {
@@ -311,14 +435,10 @@ uint32_t convert_axis_system_y(uint32_t touch_x) {
 	return ILI9488_LCD_HEIGHT*touch_x/4096;
 }
 
-void update_screen(uint32_t tx, uint32_t ty) {
-	if(tx >= BUTTON_X-BUTTON_W/2 && tx <= BUTTON_X + BUTTON_W/2) {
-		if(ty >= BUTTON_Y-BUTTON_H/2 && ty <= BUTTON_Y) {
-			draw_button(1);
-		} else if(ty > BUTTON_Y && ty < BUTTON_Y + BUTTON_H/2) {
-			draw_button(0);
-		}
-	}
+void update_screen() {
+	ili9488_draw_filled_rectangle(100, 50, 200,  200);
+	sprintf(bufferPotencia, "%d", valor_potencia_convertida);
+	font_draw_text(&digital52, bufferPotencia, 100, 50, 1);
 }
 
 void mxt_handler(struct mxt_device *device, uint *x, uint *y)
@@ -379,19 +499,40 @@ void task_mxt(void){
 }
 
 void task_lcd(void){
+  const TickType_t xDelay = 4000 / portTICK_PERIOD_MS;
+
   xQueueTouch = xQueueCreate( 10, sizeof( touchData ) );
 	configure_lcd();
   
   draw_screen();
-  draw_button(0);
+  
+  //draw_button(0);
+  // Escreve HH:MM no LCD
+  font_draw_text(&digital52, "HH:MM", 10, 400, 1);
+  font_draw_text(&digital52, "24", 100, 50, 1);
+  font_draw_text(&digital52, "100%", 100, 250, 1);
+
+
   touchData touch;
     
-  while (true) {  
-     if (xQueueReceive( xQueueTouch, &(touch), ( TickType_t )  500 / portTICK_PERIOD_MS)) {
-       update_screen(touch.x, touch.y);
-       printf("x:%d y:%d\n", touch.x, touch.y);
-     }     
-  }	 
+  while(true){  
+	   update_screen();
+       vTaskDelay(xDelay);
+     }     	 
+}
+
+/**
+ * \brief This task, when activated, make LED blink at a fixed rate
+ */
+void task_potenciometro(void *pvParameters)
+{
+	const TickType_t xDelay = 4000 / portTICK_PERIOD_MS;
+	UNUSED(pvParameters);
+	while (true) {
+		AFEC_pot_callback();
+		valor_potencia_convertida = convert_adc_to_pot(pot_ul_value);
+		vTaskDelay(xDelay);
+	}
 }
 
 /************************************************************************/
@@ -410,7 +551,14 @@ int main(void)
 
 	sysclk_init(); /* Initialize system clocks */
 	board_init();  /* Initialize board */
-	
+	ioport_init();
+
+	delay_init(sysclk_get_cpu_hz());
+
+	config_POT();
+
+	afec_start_software_conversion(AFEC0);
+
 	/* Initialize stdio on USART */
 	stdio_serial_init(USART_SERIAL_EXAMPLE, &usart_serial_options);
 		
@@ -424,6 +572,10 @@ int main(void)
     printf("Failed to create test led task\r\n");
   }
 
+ /* Create task to handler Potenciometro */
+ if (xTaskCreate(task_potenciometro, "potenciometro", TASK_POTENCIOMETRO_STACK_SIZE, NULL, TASK_POTENCIOMETRO_STACK_PRIORITY, NULL) != pdPASS) {
+	 printf("Failed to create test potenciometro task\r\n");
+ }
   /* Start the scheduler. */
   vTaskStartScheduler();
 
